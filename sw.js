dceaf1b57eee2bbf9ce06b093154333825bbdd12
helmyhelmy44-stack <helmy.helmy44@gmail.com>
2026-07-27 00:41:06 +0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'ai-came-v1';
+const ASSETS = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
